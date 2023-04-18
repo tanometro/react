24644ecd966e325ae89bdfd26c6d48a44d8737de
Angelo <angelo100492@gmail.com>
2023-04-17 22:50:01 -0300

    Componentes Formulario + select

--- src/components/formulario/formulario.js
+++ src/components/formulario/formulario.js
@@ -1,9 +1,16 @@
 import "./formulario.css"
+import CampoTexto from "../campo_texto/campo_texto.js"
+import ListaOpciones from "../lista_opciones/lista_opciones.js"
+
 
 const Formulario = () => {
-    return <section>
+    return <section className="formulario">
         <form>
             <h2>Rellena el formulario para crear el colaborador.</h2>
+            <CampoTexto titulo="Nombre" placeholder="Ingresa tu nombre"/>
+            <CampoTexto titulo="Puesto" placeholder="Ingresa tu puesto"/>
+            <CampoTexto titulo="Foto" placeholder="Inserta link de foto"/>
+            <ListaOpciones/>
         </form>
     </section>
 }
---
+++ src/components/lista_opciones/lista_opciones.js
@@ -0,0 +1,24 @@
+import "./lista_opciones.css"
+
+const ListaOpciones = () => {
+    const equipos = [
+        "Programación",
+        "Front-end",
+        "Data Science",
+        "Dev-ops",
+        "UX y Diseño",
+        "Móvil",
+        "Innovación y gestión"
+    ]
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select>
+            {equipos.map( (equipos, index) => { 
+                return <option key={index}>{equipos}</option>
+            } )}
+        </select>
+    </div>
+} 
+
+export default ListaOpciones
