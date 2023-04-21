ddb453ca6d1a653f5ecb3d6297791dc85c2b6e11
Angelo <angelo100492@gmail.com>
2023-04-20 23:14:10 -0300

    UseState en app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/header/header.js';
 import Formulario from './components/formulario/formulario.js';
 import MiOrg from './components/MiOrg';
 
 function App() {
+  const [mostrarForm,actualizarMostrar] = useState(true)
   return (
     <div>
       <Header />
